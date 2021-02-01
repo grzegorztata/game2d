a28948d9267fb38a9fc6b989805d577f76f54bd1
gtata <grzegorz.tata@unitygroup.com>
2021-02-01 17:28:53 +0100

    part_9_mobPlayer

--- src/ca/vanzeben/game/entities/Player.java
+++ src/ca/vanzeben/game/entities/Player.java
@@ -1,12 +1,14 @@
 package ca.vanzeben.game.entities;
 
 import ca.vanzeben.game.InputHandler;
+import ca.vanzeben.game.gfx.Colours;
 import ca.vanzeben.game.gfx.Screen;
 import ca.vanzeben.game.level.Level;
 
 public class Player extends Mob {
 
     private InputHandler input;
+    private int colour = Colours.get(-1, 111, 145, 543);
 
     public Player(Level level, int x, int y, InputHandler input) {
         super(level, "Player", x, y, 1);
@@ -38,7 +40,18 @@ public class Player extends Mob {
     }
 
     public void render(Screen screen) {
+        int xTile = 0;
+        int yTile = 28;
 
+        int modifier = 8 * scale;
+        int xOffset = x - modifier/2;
+        int yOffset = y - modifier/2-4;
+
+        screen.render(xOffset, yOffset, xTile + yTile * 32, colour);
+        screen.render(xOffset + modifier, yOffset, (xTile + 1) + yTile * 32, colour);
+
+        screen.render(xOffset, yOffset + modifier, xTile + (yTile + 1) * 32, colour);
+        screen.render(xOffset + modifier, yOffset + modifier, (xTile + 1) + (yTile + 1) * 32, colour);
     }
 
     public boolean hasCollided(int xa, int ya) {
--- src/ca/vanzeben/game/level/Level.java
+++ src/ca/vanzeben/game/level/Level.java
@@ -1,13 +1,18 @@
 package ca.vanzeben.game.level;
 
+import ca.vanzeben.game.entities.Entity;
 import ca.vanzeben.game.gfx.Screen;
 import ca.vanzeben.game.level.tiles.Tile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Level {
 
     private byte[] tiles;
     public int width;
     public int height;
+    public List<Entity> entities = new ArrayList<Entity>();
 
     public Level(int width, int height) {
         tiles = new byte[width * height];
@@ -29,7 +34,9 @@ public class Level {
     }
 
     public void tick() {
-
+        for (Entity e : entities) {
+            e.tick();
+        }
     }
 
     public void renderTiles(Screen screen, int xOffset, int yOffset) {
@@ -47,8 +54,18 @@ public class Level {
         }
     }
 
+    public void renderEntities(Screen screen) {
+        for (Entity e : entities) {
+            e.render(screen);
+        }
+    }
+
     private Tile getTile(int x, int y) {
         if (x<0 || x>width || y<0 || y>height) return Tile.VOID;
         return Tile.tiles[tiles[x + y * width]];
+    }
+
+    public void addEntity(Entity entity) {
+        this.entities.add(entity);
     }
 }
